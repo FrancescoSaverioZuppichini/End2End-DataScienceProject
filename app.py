64ba2431107f52aaf24a1486b73882603796c395
Francesco <francesco.saverio.zuppichini@ch.pwc.com>
2020-07-07 15:11:57 +0200

    minor changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -8,11 +9,14 @@ from pathlib import Path
 from pydantic import BaseModel
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="./app/build/"), name="static")
+app.mount("/static", StaticFiles(
+    directory=os.path.dirname(os.path.realpath(__file__)) + '/app/build/'),
+    name="static")
 
 checkpoints = Path('./checkpoints')
 model = load(checkpoints / f"RandomForestRegressor-best.joblib")
 scaler = load(checkpoints / f"scaler.joblib")
+
 
 class GDPQuery(BaseModel):
     year: int
@@ -21,11 +25,11 @@ class GDPQuery(BaseModel):
     meat: float
     urban: float
     eggs: float
-    codeId:int = 0
+    codeId: int = 0
 
 
 def predict_gdp(year: int, co2: float, oil: float, meat: float, urban: float, eggs: float):
-    oil /= 1000000 # MW -> terawatt
+    oil /= 1000000  # MW -> terawatt
     x = np.array([[year, co2, oil, meat, urban, eggs]])
     x = scaler.transform(x)
     pred = model.predict(x)
@@ -37,7 +41,8 @@ def read_root():
     with open('./app/build/index.html') as f:
         return HTMLResponse(f.read())
 
+
 @app.put("/api/pred")
 def read_item(q: GDPQuery = None):
     gdp = predict_gdp(q.year, q.co2, q.oil, q.meat, q.urban, q.eggs)
-    return { 'pred': { 'gdp' : gdp[0] } }
+    return {'pred': {'gdp': gdp[0]}}
